b28d4b9a07b4eba67e6069b26c63288af86c520d
rlawnslrlawnsl <wnsgml4627@naver.com>
2021-11-23 10:07:44 +0900

    Write a code to check the remaining battery capacity

--- sensor/src/lol_publisher.py
+++ sensor/src/lol_publisher.py
@@ -2,12 +2,18 @@
 
 import rospy
 from common_msgs.msg import lol
+from common_msgs.srv import BatteryCost, BatteryCostRequest
 
 rospy.init_node('lol_publisher')
 pub = rospy.Publisher('custom_msg', lol, queue_size=1)
 custom_data = lol()
+
+
+requester = rospy.ServiceProxy('battery_cost', BatteryCost)
+
 rate = rospy.Rate(2)
 count = 0
+battery_count = 0
 alphabets_list =['Hello', 'My', 'Name', 'Is', 'Kim', 'Jun', 'Hee']
 while not rospy.is_shutdown():
     if count > 6:
@@ -25,4 +31,18 @@ while not rospy.is_shutdown():
     print "Published geometry quaternion w: ", custom_data.quat.w
     print "Finished publishing"
     count += 1
+
+    battery_cost = 4 * battery_count
+    if battery_cost % 5 == 0:
+        request = BatteryCostRequest(cost=battery_cost)
+        response = requester(request)
+        print "================================Service Test=========================="
+        print "Battery cost : ", request.cost
+        print "Remained battery : ", response.remain
+        if response.remain == 0:
+            print "Battery needs to be charged"
+        print "======================================================================"
+    battery_count += 1
+    if battery_count > 25:
+        battery_count = 0
     rate.sleep()
